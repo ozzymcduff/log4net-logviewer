1f7694d9a48a8bf77d48a69945f110b120c13c11
oge <oskar.gewalli@lundalogik.se>
2012-04-11 09:25:56 +0200

    LogTail : simple tail of log4net or log4j logs.

---
+++ LogTail/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Core;
+
+namespace LogTail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var entries = new LogEntryParser().Parse(File.ReadAllText(args[0]));
+            var tailnum = 10;
+            if (!string.IsNullOrEmpty(args[1])) 
+            {
+                tailnum = Int32.Parse(args[1]);
+            }
+            foreach (var logEntry in entries.Skip(entries.Count()-tailnum))
+            {
+                Console.WriteLine(logEntry.Message);
+            }
+        }
+    }
+}
