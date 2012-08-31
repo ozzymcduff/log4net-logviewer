181ddf5305d05474a323c006b5c7b763c55c8082
oge <oskar.gewalli@lundalogik.se>
2012-08-31 17:24:41 +0200

    Changes to logtail...

--- LogTail/Program.cs
+++ LogTail/Program.cs
@@ -1,28 +1,112 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Core;
 using LogViewer;
+using NDesk.Options;
 
 namespace LogTail
 {
     class Program
     {
-        static void Main(string[] args)
+        class Watchers
         {
-            using (var file = FileUtil.OpenReadOnly(args[0]))
+            private readonly IEnumerable<string> _files;
+            private List<FileSystemWatcher> watchers;
+
+            public Watchers(IEnumerable<string> files)
             {
-                var items = new LogEntryParser().Parse(file)
-                    .ToArray();
-                var tailnum = 10;
-                if (args.Length >= 2 && !string.IsNullOrEmpty(args[1]))
+                _files = files;
+            }
+
+            public void Init()
+            {
+                watchers = new List<FileSystemWatcher>();
+                foreach (var path in _files.Select(Path.GetDirectoryName).Distinct())
                 {
-                    tailnum = Int32.Parse(args[1]);
+                    var watcher = new FileSystemWatcher
+                                       {
+                                           Path = path,
+                                           NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+                                       };
+                    watcher.Changed += FileHasChanged;
+                    watchers.Add(watcher);
                 }
-                foreach (var logEntry in items.Skip(items.Count() - tailnum))
+            }
+            private void FileHasChanged(object sender, FileSystemEventArgs e)
+            {
+                foreach (var fileName in _files)
                 {
-                    Console.WriteLine(logEntry.Message);
+                    if (Path.GetFullPath(e.FullPath).Equals(Path.GetFullPath(fileName),
+                                          StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        throw new NotImplementedException();
+                    }
                 }
             }
         }
+
+        static void Main(string[] args)
+        {
+            var files = new List<string>();
+            bool watch = false, monitor = false;
+            int lines = 10;
+            var p = new OptionSet() {
+                { "f|file=",   v => { files.Add (v); } },
+                { "w|watch=", v=> { watch=true;}},
+                { "m|monitor=", v=> { monitor=true;}},
+                { "l|lines=", v=> { lines=Int32.Parse(v);}}
+            };
+
+            if (args.Length > 0 && !(args[0].StartsWith("-") || args[0].StartsWith("/")))
+            {
+                files.Add(args[0]);
+            }
+
+            p.Parse(args);
+            if (watch)
+            {
+                InitWatch(files);
+            }
+            else if (monitor)
+            {
+                InitMonitor(files);
+            }
+            else
+            {
+                TailFiles(lines, files);
+            }
+
+        }
+
+        private static void InitMonitor(List<string> files)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void TailFiles(int lines, List<string> files)
+        {
+            foreach (var fileName in files)
+            {
+                using (var file = FileUtil.OpenReadOnly(fileName))
+                {
+                    var items = new LogEntryParser().Parse(file)
+                        .ToArray();
+                    foreach (var logEntry in items.Skip(items.Count() - lines))
+                    {
+                        Console.WriteLine(logEntry.Message);
+                    }
+                }
+            }
+        }
+
+        private static void InitWatch(List<string> files)
+        {
+            var w = new Watchers(files);
+            w.Init();
+        }
+
+        
     }
 }
