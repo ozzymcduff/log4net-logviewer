a004889f6b4fd6e5101fcde73b3c9ef3476485d6
oge <oskar.gewalli@lundalogik.se>
2013-04-25 16:00:22 +0200

    Package restore

--- rakefile.rb
+++ rakefile.rb
@@ -15,6 +15,21 @@ namespace :logviewer do
       nunit.command = File.join(dir,"packages/NUnit.2.5.9.10348/Tools/nunit-console-x86.exe")
       nunit.assemblies File.join(dir,"Tests/bin/Debug/Tests.dll")
     end
+
+    task :core_copy_to_nuspec => [:build] do
+      output_directory_lib = File.join(dir,"nuget/lib/40/")
+      mkdir_p output_directory_lib
+      cp Dir.glob("./Core/bin/Debug/log4net-logviewer.core.dll"), output_directory_lib
+    end
+
+    desc "create the nuget package"
+    task :nugetpack => [:core_nugetpack]
+
+    task :core_nugetpack => [:core_copy_to_nuspec] do |nuget|
+      cd File.join(dir,"nuget") do
+        sh "..\\.nuget\\NuGet.exe pack log4net-logviewer.nuspec"
+      end
+    end
   end
 
 
