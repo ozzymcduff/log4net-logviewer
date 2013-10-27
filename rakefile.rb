afc25dc1ba3ed05b20288fbc7f7ac362440c5844
Oskar Gewalli <oskar@gewalli.se>
2013-10-27 17:36:21 +0100

    Moved src to src folder

--- rakefile.rb
+++ rakefile.rb
@@ -1,54 +1,56 @@
 require 'albacore'
+desc "Nuget pack"
+task :nugetpack => ["logviewer:ms:nugetpack"]
 namespace :logviewer do
-  dir = File.dirname(__FILE__)
-  
+  $dir = File.join(File.dirname(__FILE__),'src')
+  $nuget = File.join(File.dirname(__FILE__),'nuget')
   namespace :ms do
     desc "build using msbuild"
     msbuild :build do |msb|
       msb.properties :configuration => :Debug
       msb.targets :Clean, :Rebuild
       msb.verbosity = 'quiet'
-      msb.solution = File.join(dir, "LogViewer.sln")
+      msb.solution = File.join($dir, "LogViewer.sln")
     end
     desc "test using nunit console"
     nunit :test => :build do |nunit|
-      nunit.command = File.join(dir,"packages/NUnit.2.5.9.10348/Tools/nunit-console-x86.exe")
-      nunit.assemblies File.join(dir,"Tests/bin/Debug/Tests.dll")
+      nunit.command = File.join($dir,"packages/NUnit.2.5.9.10348/Tools/nunit-console-x86.exe")
+      nunit.assemblies File.join($dir,"Tests/bin/Debug/Tests.dll")
     end
 
     task :core_copy_to_nuspec => [:build] do
-      output_directory_lib = File.join(dir,"nuget/lib/40/")
+      output_directory_lib = File.join($nuget,"lib/40/")
       mkdir_p output_directory_lib
-      cp Dir.glob("./Core/bin/Debug/log4net-logviewer.core.dll"), output_directory_lib
+      cp Dir.glob(File.join($dir, "Core/bin/Debug/log4net-logviewer.core.dll")), output_directory_lib
     end
 
     desc "create the nuget package"
     task :nugetpack => [:core_nugetpack]
 
     task :core_nugetpack => [:core_copy_to_nuspec, :runners_copy_to_nuspec] do |nuget|
-      cd File.join(dir,"nuget") do
-        sh "..\\.nuget\\NuGet.exe pack log4net-logviewer.nuspec"
+      cd File.join($nuget) do
+        sh "..\\src\\.nuget\\NuGet.exe pack log4net-logviewer.nuspec"
       end
     end
 
     task :runners_copy_to_nuspec => [:build] do
-      output_directory_lib = File.join(dir,"nuget/tools/")
+      output_directory_lib = File.join($nuget,"tools/")
       mkdir_p output_directory_lib
       ['LogTail', 'LogViewer'].each{ |project|
-        cp Dir.glob("./#{project}/bin/Debug/#{project}.exe"), output_directory_lib
+        cp Dir.glob(File.join($dir,"#{project}/bin/Debug/#{project}.exe")), output_directory_lib
       }
-      cp Dir.glob("./LogTail/bin/Debug/log4net.dll"), output_directory_lib
+      cp Dir.glob(File.join($dir,"LogTail/bin/Debug/log4net.dll")), output_directory_lib
 end
 
     task :clean_packages do
-      rm_r(File.join(dir,"nuget/tools/"))
-      rm_r(File.join(dir,"nuget/lib/"))
+      rm_r(File.join($nuget,"tools/"))
+      rm_r(File.join($nuget,"lib/"))
     end
 
     desc "install missing nuget packages"
     task :packages do
       FileList["**/packages.config"].each { |filepath|
-        sh ".\\.nuget\\NuGet.exe i #{filepath} -o ./packages"
+        sh ".\\src\\.nuget\\NuGet.exe i #{filepath} -o ./packages"
       }
     end
   end
