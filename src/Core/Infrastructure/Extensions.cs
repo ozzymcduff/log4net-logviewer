4b008a9bfa59c579c4fa1b5e30ae9459aff5fe36
Oskar Gewalli <oskar@gewalli.se>
2015-03-15 08:20:43 +0100

    ReturnsTrue

--- src/Core/Infrastructure/Extensions.cs
+++ src/Core/Infrastructure/Extensions.cs
@@ -5,10 +5,10 @@ namespace LogViewer.Infrastructure
 {
     public static class Extensions
     {
-        private static bool True<T>(T element) { return true; }
+        private static bool ReturnsTrue<T>(T element) { return true; }
         public static T Next<T>(this IList<T> that, int index, Func<T, bool> accept = null)
         {
-            if (null == accept) accept = True<T>;
+            if (null == accept) accept = ReturnsTrue<T>;
             for (int i = index + 1; i < that.Count; i++)
             {
                 var item = that[i];
@@ -19,7 +19,7 @@ namespace LogViewer.Infrastructure
         }
         public static T Previous<T>(this IList<T> that, int index, Func<T, bool> accept=null)
         {
-            if (null == accept) accept = True<T>;
+            if (null == accept) accept = ReturnsTrue<T>;
             for (int i = index - 1; 0 <= i; i--)
             {
                 var item = that[i];
